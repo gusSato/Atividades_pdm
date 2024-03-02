2e54293dbe98465e80feae7eb44c9b60c73997d0
gusSato <gustavo.o.sato@gmail.com>
2024-03-01 22:20:41 -0300

    adicao card pedidos

--- componentes-props/src/index.js
+++ componentes-props/src/index.js
@@ -13,16 +13,65 @@ const App = () => {
         <div className="row">
             {/* primeiro pedido */}
             <div className="col-sm-12 col-lg-6 col-xxl-3">
-                <div className="bg-secondary">caixa1</div>
+                {/* .card>.card-header.text-muted{22/04/2024}+.card-body.d-flex */}
+                <div className="card">
+                    <div className="card-header text-muted">22/04/2021</div>
+                    <div className="card-body d-flex">
+                        <div className="d-flex align-items-center">
+                            <i className="fa-regular fa-hard-drive fa-2x fa-beat-fade"></i>
+                        </div>
+                        <div className="flex-grow-1 ms-3 border">
+                            <h4 className="text-center">HD</h4>
+                            <p className="text-center">HD 1Tb</p>
+                        </div>
+                    </div>
+                </div>
             </div>
+
             <div className="col-sm-12 col-lg-6 col-xxl-3">
-                <div className="bg-secondary">caixa2</div>
+                <div className="card">
+                    <div className="card-header text-muted">22/04/2021</div>
+                    <div className="card-body d-flex">
+                        <div className="d-flex align-items-center">
+                            <i className="fa-solid fa-memory fa-2x fa-bounce"></i>
+                        </div>
+                        <div className="flex-grow-1 ms-3 border">
+                            <h4 className="text-center">RAM</h4>
+                            <p className="text-center">Memória 16gb</p>
+                        </div>
+                    </div>
+                </div>
             </div>
+
             <div className="col-sm-12 col-lg-6 col-xxl-3">
-                <div className="bg-secondary">caixa3</div>
+                <div className="card">
+                    <div className="card-header text-muted">22/04/2021</div>
+                    <div className="card-body d-flex">
+                        <div className="d-flex align-items-center">
+                            <i className="fa-brands fa-java fa-2x fa-flip"></i>
+                        </div>
+                        <div className="flex-grow-1 ms-3 border">
+                            <h4 className="text-center">Java</h4>
+                            <p className="text-center">Projetin felas</p>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+
             <div className="col-sm-12 col-lg-6 col-xxl-3">
-                <div className="bg-secondary">caixa4</div>
+                <div className="card">
+                    <div className="card-header text-muted">22/04/2021</div>
+                    <div className="card-body d-flex">
+                        <div className="d-flex align-items-center">
+                            <i className="fa-solid fa-gamepad fa-2x fa-shake"></i>
+                        </div>
+                        <div className="flex-grow-1 ms-3 border">
+                            <h4 className="text-center">RAM</h4>
+                            <p className="text-center">Memória 16gb</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <div className="row">
