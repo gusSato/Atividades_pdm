e7879bad0a1a8bd0795f74a4c0b34c516d35f1eb
gusSato <gustavo.o.sato@gmail.com>
2024-03-15 21:24:57 -0300

    feat(ap05): encerra a secao 2.7
    - cria os componentes

---
+++ componentes-props/src/Empresa.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Empresa = () => {
+  return (
+    <div className="card">
+      <H1>{props.nome}</H1>
+    </div>
+  )
+}
+
+export default Empresa
---
+++ estacao-climatica/src/index.js
@@ -0,0 +1,60 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component{
+  // window.navigator.geolocation.getCurrentPosition((position) => {
+  //   console.log(position)
+
+  constructor(props){
+    super(props)
+    this.state = {
+      latitude: null,
+      longitude: null,
+      estacao: null,
+      data: null, 
+      icone: null
+    }
+  }
+
+  icones(){
+    'verao' = 'sun'
+  }
+
+  obterEstacao = (dataAtual, latitude) =>{
+    const anoAtual = dataAtual.getFullYear()
+    //21/06
+    //new Date
+    const d1 = new Date(anoAtual, 5, 21)
+    //24/09
+    const d2 = new Date(anoAtual, 8, 24)
+    //22/12
+    const d3 = new Date(anoAtual, 11, 22)
+    //21/03
+    const d4 = new Date(anoAtual, 2, 21)
+    const sul = latitude < 0 ? true : false
+    if (dataAtual >= d1 && dataAtual < d2)
+      return sul ? 'Inverno' : 'Verão'
+    if (dataAtual >= d2 && dataAtual < d3)
+      return sul ? 'Primavera' : 'Outono'
+    if (dataAtual >= d3 || dataAtual < d4)
+      return sul ? 'Verão' : 'Inverno'
+    return sul ? 'Outono' : 'Primavera'
+  }
+
+  obterEstacao = () => {
+
+  }
+
+  render(){
+    return <div>
+      Oi
+    </div>
+  }
+}
+
+ReactDOM.render(
+  <App/>,
+  document.querySelector('#root')
+)
